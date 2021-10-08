0f5b04a9e2d914b1a3ce745d18da72ded0e204c4
tobiasreinbold linux <tobiasreinbold1@gmail.com>
2021-10-08 17:58:25 +0200

    fixed css build

--- src/styled/index.ts
+++ src/styled/index.ts
@@ -4,5 +4,6 @@ import Container from "./Container/Container";
 import Dropdown from "./Dropdown/Dropdown";
 import Frame from "./Frame/Frame";
 import Tabs from "./Tabs/Tabs";
+import "./main.css";
 
 export default { Button, Card, Container, Dropdown, Frame, Tabs };
